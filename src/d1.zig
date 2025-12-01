3c3a43dcbc6a416160db080e74e21d1cddcc4801
zac <zac@zacoons.com>
2025-12-01 22:04:47 +1000

    day 1 part 2

--- src/d1.zig
+++ src/d1.zig
@@ -4,34 +4,38 @@ const alloc = std.heap.smp_allocator;
 
 pub fn main() !void {
     var r: i16 = 50;
-    var passwd: u16 = 0;
+    var passwd: i16 = 0;
 
     const file = try std.fs.cwd().openFile("data/d1.txt", .{ .mode = .read_only });
     const readbuf = try alloc.alloc(u8, 512);
     var reader = file.reader(readbuf);
 
-    var linebuf: [4]u8 = undefined;
-
     while (true) {
-        var linebuf_w = std.Io.Writer.fixed(&linebuf);
-        const n = reader.interface.streamDelimiter(&linebuf_w, '\n') catch |err| switch (err) {
+        const line_with_delim = reader.interface.takeDelimiterInclusive('\n') catch |err| switch (err) {
             error.EndOfStream => break,
             else => return err,
         };
-        reader.interface.toss(1); // Skip over the delimiter itself
-        const line = linebuf[0..n];
+        const line = line_with_delim[0 .. line_with_delim.len - 1];
         const number = try std.fmt.parseInt(i16, line[1..], 10);
         switch (line[0]) {
             'L' => {
+                if (r == 0) passwd -= 1;
                 r = r - number;
-                while (r < 0) r += 100;
+                while (r < 0) {
+                    passwd += 1;
+                    r += 100;
+                }
             },
             'R' => {
-                r = @mod(r + number, 100);
+                r = r + number;
+                while (r >= 100) {
+                    passwd += 1;
+                    r -= 100;
+                }
+                if (r == 0) passwd -= 1;
             },
             else => return error.InvalidFormat,
         }
-
         if (r == 0) {
             passwd += 1;
         }
