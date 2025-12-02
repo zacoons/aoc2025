4f6558a3f292e330b62c593bba7b7738227f7467
zac <zac@zacoons.com>
2025-12-02 20:46:44 +1000

    day 2 part 2

--- src/d2.zig
+++ src/d2.zig
@@ -11,6 +11,7 @@ pub fn main() !void {
 
     var range_str_buf: [64]u8 = undefined;
     var id_str_buf: [32]u8 = undefined;
+    var pattern_buf: [32]u8 = undefined;
 
     while (true) {
         var range_str_buf_w = std.Io.Writer.fixed(&range_str_buf);
@@ -25,13 +26,25 @@ pub fn main() !void {
         const right = try std.fmt.parseInt(usize, range_str[hyphen_idx + 1 ..], 10);
         for (left..right + 1) |id| {
             const id_str_len = std.fmt.printInt(&id_str_buf, id, 10, .lower, .{});
+            if (id_str_len == 1) continue;
             const id_str = id_str_buf[0..id_str_len];
-            if (id_str.len % 2 != 0) continue;
-            const half_id_str_len = @divExact(id_str_len, 2);
-            const id_str_left = id_str[0..half_id_str_len];
-            const id_str_right = id_str[half_id_str_len..];
-            if (std.mem.eql(u8, id_str_left, id_str_right)) {
-                count += id;
+
+            const half_id_str_len = @divTrunc(id_str_len, 2);
+            for (1..half_id_str_len + 1) |pattern_size| {
+                if (id_str_len % pattern_size != 0) continue;
+                const max_pattern_match_count = id_str_len / pattern_size;
+                for (0..pattern_size) |i| pattern_buf[i] = id_str[i];
+                var pattern_match_count: u8 = 0;
+                var i: usize = 0;
+                while (i < id_str_len) : (i += pattern_size) {
+                    if (std.mem.eql(u8, id_str[i .. i + pattern_size], pattern_buf[0..pattern_size])) {
+                        pattern_match_count += 1;
+                    }
+                }
+                if (pattern_match_count == max_pattern_match_count) {
+                    count += id;
+                    break;
+                }
             }
         }
     }
