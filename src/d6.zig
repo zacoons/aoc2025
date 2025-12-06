075d6d0c17c4ef72a821a6cbb2b98cb6278c91d5
zac <zac@zacoons.com>
2025-12-06 18:25:31 +1000

    day 6 part 1 reworked

--- src/d6.zig
+++ src/d6.zig
@@ -2,76 +2,47 @@ const std = @import("std");
 
 const alloc = std.heap.smp_allocator;
 
-const Symbol = enum {
-    add,
-    mult,
+const Col = struct {
+    start_x: usize,
+    end_x: usize,
+    op: u8,
 };
 
 pub fn main() !void {
-    const file = try std.fs.cwd().openFile("data/d6.txt", .{ .mode = .read_only });
-    const readbuf = try alloc.alloc(u8, 512);
-    var reader = file.reader(readbuf);
+    const file = try std.fs.cwd().readFileAlloc(alloc, "data/d6.txt", 1024 * 1024);
+    const w = std.mem.indexOfScalar(u8, file, '\n').? + 1;
+    const h = @divExact(file.len, w);
 
-    var linebuf: [1024 * 1024]u8 = undefined;
-
-    var rows: std.ArrayList([]u64) = try .initCapacity(alloc, 4);
-    var symbols: []Symbol = undefined;
-
-    while (true) {
-        var linebuf_w = std.Io.Writer.fixed(&linebuf);
-        const n = try reader.interface.streamDelimiterEnding(&linebuf_w, '\n');
-        if (n == 0) break;
-        reader.interface.toss(1); // Skip over the delimiter itself
-        const line = linebuf[0..n];
-
-        if (line[0] == '+' or line[0] == '*') {
-            var row: std.ArrayList(Symbol) = try .initCapacity(alloc, 4);
-
-            var start_i: usize = 0;
-            while (std.mem.indexOfAnyPos(u8, line, start_i, &.{ '+', '*' })) |i| {
-                start_i = i + 1;
-                const symbol: Symbol = switch (line[i]) {
-                    '+' => .add,
-                    '*' => .mult,
-                    else => return error.InvalidSymbol,
-                };
-                try row.append(alloc, symbol);
-            }
-
-            symbols = try row.toOwnedSlice(alloc);
-        } else {
-            var row: std.ArrayList(u64) = try .initCapacity(alloc, 4);
-
-            var start_index: usize = std.mem.indexOfNone(u8, line, &.{' '}).?;
-            var end_index: usize = undefined;
-            while (true) {
-                end_index = std.mem.indexOfScalarPos(u8, line, start_index + 1, ' ') orelse n;
-                const num = try std.fmt.parseInt(u64, line[start_index..end_index], 10);
-                try row.append(alloc, num);
-                if (end_index == n) break;
-                start_index = std.mem.indexOfNonePos(u8, line, end_index + 1, &.{' '}) orelse break;
-            }
-
-            try rows.append(alloc, try row.toOwnedSlice(alloc));
+    var cols = try std.ArrayList(Col).initCapacity(alloc, 4);
+    {
+        const offset = (h - 1) * w; // This is the index at which the last row starts
+        var start_i = offset;
+        while (true) {
+            const i = std.mem.indexOfAnyPos(u8, file, start_i + 1, &.{ '+', '*' }) orelse file.len - 1;
+            try cols.append(alloc, .{
+                .start_x = start_i - offset,
+                .end_x = if (i == file.len - 1) i - offset else i - offset - 1,
+                .op = file[start_i],
+            });
+            if (i == file.len - 1) break;
+            start_i = i;
         }
     }
 
-    std.debug.print("{any}\n", .{rows});
-    std.debug.print("{any}\n", .{symbols});
-
     var sum: u64 = 0;
 
-    for (0..rows.items[0].len) |col| {
-        const symbol = symbols[col];
-        var local_sum: u64 = 0;
-        switch (symbol) {
-            .add => {
-                for (rows.items) |row| local_sum += row[col];
-            },
-            .mult => {
-                local_sum = 1;
-                for (rows.items) |row| local_sum *= row[col];
-            },
+    for (cols.items) |col| {
+        const col_w = col.end_x - col.start_x;
+        var local_sum: u64 = if (col.op == '*') 1 else 0;
+        var i = col.start_x;
+        while (i < file.len - w) : (i += w) {
+            const num_str = std.mem.trim(u8, file[i .. i + col_w], &.{' '});
+            const num = try std.fmt.parseInt(u64, num_str, 10);
+            switch (col.op) {
+                '+' => local_sum += num,
+                '*' => local_sum *= num,
+                else => return error.InvalidOperator,
+            }
         }
         sum += local_sum;
     }
