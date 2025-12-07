df8171095f9192f1f017252fd240999f722dd8eb
zac <zac@zacoons.com>
2025-12-08 08:02:23 +1000

    day 7 part 2

--- src/d7.zig
+++ src/d7.zig
@@ -2,29 +2,29 @@ const std = @import("std");
 
 const alloc = std.heap.smp_allocator;
 
+fn count(cache: *std.AutoHashMap(usize, u128), file: []u8, w: usize, start_x: usize, start_y: usize) !u128 {
+    const cache_key = w * start_y + start_x;
+    if (cache.get(cache_key)) |c| return c;
+    var i = cache_key;
+    while (i < file.len) : (i += w) {
+        if (file[i] == '^') {
+            var c: u128 = 1;
+            c += try count(cache, file, w, start_x - 1, @divTrunc(i, w));
+            c += try count(cache, file, w, start_x + 1, @divTrunc(i, w));
+            try cache.put(cache_key, c);
+            return c;
+        }
+    }
+    return 0;
+}
+
 pub fn main() !void {
     var timer = try std.time.Timer.start();
 
     const file = try std.fs.cwd().readFileAlloc(alloc, "data/d7.txt", 1024 * 1024);
     const w = std.mem.indexOfScalar(u8, file, '\n').? + 1;
 
-    var count: u32 = 0;
-
-    const beams = try alloc.alloc(bool, w);
-    beams[std.mem.indexOfScalar(u8, file, 'S').?] = true;
-    var i: usize = w;
-    while (i < file.len) : (i += w) {
-        const line = file[i .. i + w];
-        var start_x: usize = 0;
-        while (std.mem.indexOfScalarPos(u8, line, start_x, '^')) |splitter_x| : (start_x = splitter_x + 1) {
-            if (beams[splitter_x]) {
-                beams[splitter_x] = false;
-                beams[splitter_x - 1] = true;
-                beams[splitter_x + 1] = true;
-                count += 1;
-            }
-        }
-    }
-
-    std.debug.print("{} {D}\n", .{ count, timer.read() });
+    var cache = std.AutoHashMap(usize, u128).init(alloc);
+    const start_x = std.mem.indexOfScalar(u8, file, 'S').?;
+    std.debug.print("{} {D}\n", .{ 1 + try count(&cache, file, w, start_x, 1), timer.read() });
 }
